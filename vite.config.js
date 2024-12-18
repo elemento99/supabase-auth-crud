98dcdd8a11c7a88dc08bfbee2e2fddb68999aa3e
hajime2 <joaquin.gonzalezparada@gmail.com>
2024-12-18 15:40:16 -0300

    deployign

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
+  
   plugins: [react()],
   build: {
     sourcemap: mode === 'development', // Activa source maps solo en desarrollo
   },
+  base: '/supabase-auth-crud/',
 }));
